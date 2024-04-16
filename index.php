5befb659c9b0cfd67f6bdf5ff399252bcd607971
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-16 14:32:12 +0200

    [Init] First Commit + Readme File

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$prova = "Questa è una prova";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+</head>
+<body>
+    <div><?php echo $prova ?></div>
+</body>
+</html>
