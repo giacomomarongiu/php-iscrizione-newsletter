1ec2b67613e02330527fa4b883cc8aedca82fa9b
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-16 17:13:13 +0200

    [Exploring PHP] Cut off the top of the page and included with head.php via include_once

---
+++ layouts/head.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3a46370e2f.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Newsletter</title>
+</head>
+
+<body>
+
+    <!--Navbar-->
+    <header class="mb-3">
+        <nav class="navbar bg-primary">
+            <div class="container-fluid justify-content-center">
+                <a class="navbar-brand" href="">
+                    Newsletter <i class="fa-solid fa-envelope"></i>
+                </a>
+            </div>
+        </nav>
+    </header>
--- success.php
+++ success.php
@@ -2,36 +2,11 @@
 session_start();
 #var_dump($_SESSION);
 $message = ($_SESSION['message']);
+
+include_once __DIR__ . '/layouts/head.php';
 ?>
 
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/3a46370e2f.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Good Job! - Newsletter</title>
-</head>
-
-<body>
-
-    <!--Navbar-->
-    <header class="mb-3">
-        <nav class="navbar bg-primary">
-            <div class="container-fluid justify-content-center">
-                <a class="navbar-brand" href="#">
-                    Newsletter <i class="fa-solid fa-envelope"></i>
-                </a>
-            </div>
-        </nav>
-    </header>
 
 
     <div class="container">
